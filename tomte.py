bbfad17e642e1d10b0715c438a752025edc2dbdb
albipa <albion@paulsson.net>
2021-12-15 13:43:54 +0100

    asjdasdasasdasdasd

--- tomte.py
+++ tomte.py
@@ -1,17 +1,19 @@
 import time
 import os
 import random
+from typing import Collection
 
-def clearConsole():
+def clearconsole():
     command = 'clear'
     if os.name in ('nt', 'dos'):
         command = 'cls'
     os.system(command)
 
 def new():
+    clearconsole()
     filename = input("Please input file name: ")
     filename = filename + ".txt"
-    with open(filename,"w", encoding="utf8") as f:
+    with open(f"{filename}","w", encoding="utf8") as f:
         recievername = input("Please Input name of reciever: ")
         f.write(f"-List of {recievername}-\n")
         while True:
@@ -22,6 +24,7 @@ def new():
                 f.write(f"{present}\n")
 
 def read():
+    clearconsole()
     filename = input("Please input file name: ")
     filename = filename + ".txt"
     with open(f"{filename}", "r", encoding="utf8") as f:
@@ -30,14 +33,17 @@ def read():
     # f.readline ger en rad i taget
         for line in f.readlines():
             print(line)
+        input("Press enter to return ")
+        clearconsole()
 
 def start():
+    clearconsole()
     load = ("Initializing")
     for i in range(4):
         print(load)
         load += "."
         time.sleep(1)
-        clearConsole()
+        clearconsole()
     while True:
         menuchoice = input("""
 | WishList9000.exe
@@ -46,7 +52,7 @@ def start():
 |
 | >Read List
 |
-| ©Santa Inc, 1987
+| © Santa Inc, 1987
 |
 | Choose Option [New/Read/Quit]: """)
 
